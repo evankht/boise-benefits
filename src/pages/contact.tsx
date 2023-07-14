0f92085df552cba8ee4051d14372788bebc392bb
Evan <evan@kittyhawk.tech>
2023-07-14 04:02:52 -0600

    fix contact page to be a valid component

---
+++ src/pages/contact.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { type NextPage } from "next";
+
+export const Contact: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Boise Benefits</title>
+        <meta name="description" content="Boise Benefits" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <main>
+        <h1>About</h1>
+      </main>
+    </>
+  );
+};
+
+export default Contact;
